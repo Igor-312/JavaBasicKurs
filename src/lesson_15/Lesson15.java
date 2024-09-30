1bf4e57297ed7b6fc9c8af71667d7643266738d9
IgorSv <igorexa08@gmail.com>
2024-09-30 12:55:25 +0200

    Lesson15

--- src/lesson_15/Lesson15.java
+++
@@ -1,4 +0,0 @@
-package lesson_15;
-
-public class Lesson15 {
-}
---
+++ src/lesson_15/MainCats.java
@@ -0,0 +1,34 @@
+package lesson_15;
+
+public class MainCats {
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+
+
+        cat.sayMeow();
+        cat.run();
+        cat.sleep();
+
+        String catName = cat.name;
+        System.out.println("Name Cat: " + catName);
+
+        System.out.println("Age: " + cat.age);
+
+        System.out.println("===================\n");
+
+        Cat cat1 = new Cat("Tom");
+
+        System.out.println("Color: " + cat1.color);
+        System.out.println("Name Cat: " + cat1.name);
+
+        System.out.println("=====================\n");
+        Cat cat2 = new Cat("Tomas", "Red");
+
+        cat2.whoAmI();
+        System.out.println("=====================\n");
+        Cat barsik = new Cat("Barsik", "White", 3);
+        barsik.whoAmI();
+
+    }
+}
