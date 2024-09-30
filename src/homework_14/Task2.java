31be9ce503d92885c999fac72ec67bffec5b680a
IgorSv <igorexa08@gmail.com>
2024-09-30 11:20:17 +0200

    Lesson15

--- src/homework_14/Task2.java
+++ src/homework_14/Task2.java
@@ -8,7 +8,7 @@ Task 2
  */
 public class Task2 {
     public static void main(String[] args) {
-int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+int[] numbers = {1,2,3,4,5,6,7,8,9,};
 int sum = sumOfEvenNumbers(numbers);
         System.out.println("Сумма четных чисел: " + sum);
     }
