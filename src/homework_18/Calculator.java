bae94ca2d28b750c1cb72c9110932fe3805c422b
IgorSv <igorexa08@gmail.com>
2024-10-07 19:30:53 +0200

    homework_18 "Dog problem"

---
+++ src/homework_18/Calculator.java
@@ -0,0 +1,29 @@
+package homework_18;
+/*
+Task 1
+Класс "Калькулятор"
+Создайте класс Calculator, который будет предоставлять
+базовые арифметические операции: сложение, вычитание, умножение и деление
+Класс должен содержать:
+Статические методы для каждой математической операции, принимающие два аргумента и
+возвращающие результат операции.
+ */
+public class Calculator {
+    // сложение
+    static double add(double a, double b) {
+        return a + b;
+    }
+    // вычитание
+    static double subtract(double a, double b) {
+        return a - b;
+    }
+    // умножение
+    static double multiply(double a, double b) {
+        return a * b;
+    }
+    // деление
+    static double divide(double a, double b) {
+       return a / b;
+    }
+
+}
