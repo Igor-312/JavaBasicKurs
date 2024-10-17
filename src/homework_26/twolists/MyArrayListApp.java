60a6255f578937e5a102adcff391b776c12078f9
IgorSv <igorexa08@gmail.com>
2024-10-17 21:47:03 +0200

    homework_26 add "Task1 and Task2* option"

---
+++ src/homework_26/twolists/MyArrayListApp.java
@@ -0,0 +1,45 @@
+package homework_26.twolists;
+
+import java.util.List;
+
+/*
+Task 2. * Опционально
+Обобщенный статический метод “Печать двух списков”
+Напишите обобщенный статический метод printTwoLists, который принимает два параметра типа MyArrayList
+и MyArrayList, и выводит элементы обоих списков.
+Требования:
+Метод должен быть обобщенным по двум типам T и U.
+Элементы списков могут быть разных типов.
+ */
+public class MyArrayListApp {
+    public static <T, U> void printTwoLists(MyArrayList<T> list1, MyArrayList<U> list2) {
+        System.out.print("First list: ");
+        for (T element : list1.toArray()) {
+            System.out.print(element + ", ");
+        }
+
+        System.out.println("\n ========================== \n");
+
+        System.out.print("Second list: ");
+        for (U element : list2.toArray()) {
+            System.out.print(element);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MyArrayList<Integer> numbers = new MyArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        MyArrayList<String> words = new MyArrayList<>();
+        words.add("Hello ");
+        words.add("World ");
+        words.add("Java!");
+
+        printTwoLists(numbers, words);
+    }
+
+
+}
