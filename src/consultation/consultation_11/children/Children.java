fb81764d643fd8fd0cc9b738859a2e2f2045cbd0
IgorSv <igorexa08@gmail.com>
2024-10-20 19:49:43 +0200

    consultation add "problem"

--- src/consultation/consultation_11/children/Children.java
+++ src/consultation/consultation_11/children/Children.java
@@ -7,10 +7,16 @@
 //
 //        Parent link = new Child();
 //
+//        // Не могу вызвать метод класса Child из-за "узкого" типа ссылки
+//        // link.child();
+//
+//        // Получить реальный класс объект link
 //        Class<?> clazz = link.getClass();
 //
+//        // Получить доступ к методу
 //        Method method = clazz.getMethod("child", String.class);
 //
+//        // Вызываем метод child на объекте link
 //        method.invoke(link, "Пример строки");
 //    }
 //}
